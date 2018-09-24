71406430dbfbda696aced899c385e68a9341cc92
ktr0731 <ktr@syfm.me>
2018-09-24 18:32:45 +0900

    update web design

--- client/index.js
+++ client/index.js
@@ -1,16 +1,34 @@
 import { SimpleRequest } from '../api/api_pb';
 import { SimpleServiceClient } from "../api/api_pb_service";
 
-const req = new SimpleRequest();
-req.setName("kumiko oumae");
+const client = new SimpleServiceClient('https://localhost:50051');
 
-const e = document.querySelector("#message");
+document.querySelector("#unary-button").addEventListener("click", () => {
+    const req = new SimpleRequest();
+    req.setName("kumiko oumae");
 
-const client = new SimpleServiceClient('http://localhost:50051');
-client.unary(req, (err, res) => {
-    if (err) {
-        console.error(err);
-    } else {
+    client.unary(req, (err, res) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        const e = document.querySelector("#unary-message");
         e.innerText = res.getMessage();
-    }
+    });
+});
+
+document.querySelector("#server-stream-button").addEventListener("click", () => {
+    const req = new SimpleRequest();
+    req.setName("kumiko oumae");
+
+    const e = document.querySelector("#server-stream-message");
+    e.innerText = "";
+
+    const stream = client.serverStreaming(req);
+    stream.on("data", res => {
+        e.innerText += res.getMessage() + "\n";
+    });
+    stream.on("end", () => {
+        e.innerText += "finished";
+    })
 });
