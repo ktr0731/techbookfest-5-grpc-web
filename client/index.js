a4cddb2d21e24fd8c614df91de7390c7bb003157
ktr0731 <ktr@syfm.me>
2018-09-24 23:39:18 +0900

    add client streaming button and logic

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
+import { grpc } from 'grpc-web-client';
 import { SimpleRequest } from '../api/api_pb';
-import { SimpleServiceClient } from "../api/api_pb_service";
+import { SimpleServiceClient, SimpleService } from "../api/api_pb_service";
 
-const client = new SimpleServiceClient('https://localhost:50051');
+const host = 'https://localhost:50051';
+const client = new SimpleServiceClient(host);
 
 document.querySelector("#unary-button").addEventListener("click", () => {
     const req = new SimpleRequest();
@@ -30,5 +32,36 @@ document.querySelector("#server-stream-button").addEventListener("click", () => 
     });
     stream.on("end", () => {
         e.innerText += "finished";
+    })
+});
+
+document.querySelector("#client-stream-button").addEventListener("click", () => {
+    const e = document.querySelector("#client-stream-message");
+    e.innerText = "";
+
+    const client = grpc.client(SimpleService.ClientStreaming, {
+        host: host,
+        transport: grpc.WebsocketTransportFactory,
+    });
+    client.onMessage(message => {
+        e.innerText += message.array[0];
+    });
+
+    client.start();
+
+    const promises = ["oumae", "kousaka", "kato", "kawashima"].map((name, i) => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                e.innerText += `[${i+1}]: name = ${name}\n`;
+                const req = new SimpleRequest();
+                req.setName(name);
+                client.send(req);
+                resolve();
+            }, 500*i);
+        });
+    });
+    Promise.all(promises).then(() => {
+        client.finishSend();
+        e.innerText += "\n";
     })
 });
